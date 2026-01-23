987789644af948193d4fd4b3bc6e86958bdccd31
jclee <jclee@jclee.me>
2026-01-23 11:24:25 +0900

    chore(release): airgap package artifact only

--- frontend/app/ip-management/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types';
-export { useIPManagement } from './useIPManagement';
-export { IPManagementTabs } from './IPManagementTabs';
-export { IPManagementFilters } from './IPManagementFilters';
-export { IPManagementTable } from './IPManagementTable';
-export { IPManagementFormModal } from './IPManagementFormModal';
-export { DeleteConfirmModal } from './DeleteConfirmModal';
--- frontend/components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Button } from './Button';
-export { Card, CardHeader, StatCard } from './Card';
-export { default as EmptyState } from './EmptyState';
-export { default as Input } from './Input';
-export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as Modal } from './Modal';
-export { default as PageHeader } from './PageHeader';
-export { default as Tabs } from './Tabs';
-
